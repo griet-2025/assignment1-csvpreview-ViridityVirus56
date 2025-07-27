858eb3a3b047b0127927b847c2a05a107909a1da
ViridityVirus56 <avipuj3@gmail.com>
2025-07-27 20:51:08 +0530

    Assignment 1 23241A6110

--- task1/CSVReaderPreview.java
+++ task1/CSVReaderPreview.java
@@ -1,4 +1,4 @@
-package task1;
+// package task1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,6 +9,38 @@ public class CSVReaderPreview {
     public static void main(String[] args) {
         // TO DO: Write your code below 
         String filePath = "dataset/dataset.csv";
-        
+        String line = "";
+        System.out.println("=== Data Preview === \n\n");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String idLine = br.readLine();
+            String[] columnNames = idLine.split(",");
+            System.out.println("Columns: ");
+            for(String cName: columnNames) {
+                System.out.print(cName + "  ");
+            }
+            System.out.println();
+            int recordCount = 0;
+            int limit = 5;
+            int count = 0;
+            System.out.println("Total Columns: " + columnNames.length);
+            System.out.println("\n\nFirst " + limit + " records: \n\n");
+            while(((line = br.readLine()) != null)) {
+                String[] values = line.split(",");
+                if(count < limit) {
+                    for(String value: values) {
+                        System.out.print(value + "  ");
+                    }
+                    System.out.println();
+                }
+                recordCount++;
+                count++;
+            }
+            System.out.println("\n\nTotal Records (excluding header): " + recordCount);
+            br.close();
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
